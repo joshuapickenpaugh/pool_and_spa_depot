809518cc9cea94710ba0bf14a8217adab9e7b733
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-09-22 11:05:39 -0500

    Commit (CH21 code)

--- PoolAndSpaDepot/Form1.Designer.vb
+++ PoolAndSpaDepot/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmMain
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,151 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtLength = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtWidth = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtDepth = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(142, 237)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(163, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "&Length:"
+        '
+        'txtLength
+        '
+        Me.txtLength.Location = New System.Drawing.Point(208, 12)
+        Me.txtLength.Name = "txtLength"
+        Me.txtLength.Size = New System.Drawing.Size(100, 20)
+        Me.txtLength.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(163, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "&Width:"
+        '
+        'txtWidth
+        '
+        Me.txtWidth.Location = New System.Drawing.Point(208, 55)
+        Me.txtWidth.Name = "txtWidth"
+        Me.txtWidth.Size = New System.Drawing.Size(100, 20)
+        Me.txtWidth.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(163, 100)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "&Depth:"
+        '
+        'txtDepth
+        '
+        Me.txtDepth.Location = New System.Drawing.Point(208, 97)
+        Me.txtDepth.Name = "txtDepth"
+        Me.txtDepth.Size = New System.Drawing.Size(100, 20)
+        Me.txtDepth.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(163, 180)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(45, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Gallons:"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotal.Location = New System.Drawing.Point(208, 180)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(171, 43)
+        Me.lblTotal.TabIndex = 8
+        Me.lblTotal.Text = " "
+        Me.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(398, 8)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(75, 105)
+        Me.btnCalc.TabIndex = 9
+        Me.btnCalc.Text = "&Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(398, 126)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 123)
+        Me.btnExit.TabIndex = 10
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(494, 261)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtDepth)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtWidth)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtLength)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Pool & Spa Depot"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtLength As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtWidth As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtDepth As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnExit As Button
 End Class
--- PoolAndSpaDepot/Form1.vb
+++ PoolAndSpaDepot/Form1.vb
@@ -1,3 +1,46 @@
-﻿Public Class Form1
+﻿'Joshua Pickenpaugh
+'September 22nd, 2016
+'"Pool and Spa Depot", CPT 432, Unit 6
 
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        'Closes the app:
+        Me.Close()
+
+    End Sub
+
+    Public Function GetGallons(ByVal pool As Dimensions) As Double
+        'Calculates and returns the number of gallons.
+
+        Const dblGAL_PER_CUBIC_FOOT As Double = 7.48
+
+        Return pool.dblLength * pool.dblDepth * pool.dblWidth * dblGAL_PER_CUBIC_FOOT
+
+    End Function
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+
+        'Delcares variables:
+        Dim poolSize As Dimensions
+        Dim dblGallons As Double
+
+        'Assigns structures to text boxes:
+        Double.TryParse(txtLength.Text, poolSize.dblLength)
+        Double.TryParse(txtWidth.Text, poolSize.dblWidth)
+        Double.TryParse(txtDepth.Text, poolSize.dblDepth)
+
+        dblGallons = GetGallons(poolSize)
+        lblTotal.Text = dblGallons.ToString("N0")
+
+    End Sub
+
+    Structure Dimensions
+
+        'Instead of variables, declares it as a structure:
+        Public dblLength As Double
+        Public dblWidth As Double
+        Public dblDepth As Double
+
+    End Structure
 End Class
