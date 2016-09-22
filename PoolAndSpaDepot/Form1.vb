0d8d435512cdbe5fea8066d17252466aee340711
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-09-22 11:34:43 -0500

    Final commit (CH26 code)

--- PoolAndSpaDepot/Form1.vb
+++ PoolAndSpaDepot/Form1.vb
@@ -10,37 +10,37 @@ Public Class frmMain
 
     End Sub
 
-    Public Function GetGallons(ByVal pool As Dimensions) As Double
-        'Calculates and returns the number of gallons.
+    'Public Function GetGallons(ByVal pool As Dimensions) As Double
+    '    'Calculates and returns the number of gallons.
 
-        Const dblGAL_PER_CUBIC_FOOT As Double = 7.48
+    '    Const dblGAL_PER_CUBIC_FOOT As Double = 7.48
 
-        Return pool.dblLength * pool.dblDepth * pool.dblWidth * dblGAL_PER_CUBIC_FOOT
+    '    Return pool.dblLength * pool.dblDepth * pool.dblWidth * dblGAL_PER_CUBIC_FOOT
 
-    End Function
+    'End Function
 
     Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
 
         'Delcares variables:
-        Dim poolSize As Dimensions
+        Dim poolSize As New RectangularPool
         Dim dblGallons As Double
 
         'Assigns structures to text boxes:
-        Double.TryParse(txtLength.Text, poolSize.dblLength)
-        Double.TryParse(txtWidth.Text, poolSize.dblWidth)
-        Double.TryParse(txtDepth.Text, poolSize.dblDepth)
+        Double.TryParse(txtLength.Text, poolSize.Length)
+        Double.TryParse(txtWidth.Text, poolSize.Width)
+        Double.TryParse(txtDepth.Text, poolSize.Depth)
 
-        dblGallons = GetGallons(poolSize)
+        dblGallons = poolSize.GetGallons
         lblTotal.Text = dblGallons.ToString("N0")
 
     End Sub
 
-    Structure Dimensions
+    'Structure Dimensions
 
-        'Instead of variables, declares it as a structure:
-        Public dblLength As Double
-        Public dblWidth As Double
-        Public dblDepth As Double
+    '    'Instead of variables, declares it as a structure:
+    '    Public dblLength As Double
+    '    Public dblWidth As Double
+    '    Public dblDepth As Double
 
-    End Structure
+    'End Structure
 End Class
---
+++ PoolAndSpaDepot/RectangularPool.vb
@@ -0,0 +1,69 @@
+﻿Public Class RectangularPool
+
+    Private _dblLength As Double
+    Private _dblWidth As Double
+    Private _dblDepth As Double
+    Const dblGAL_PER_CUBIC_FOOT As Double = 7.48
+
+    'Get and Set:
+    Public Property Length As Double
+        Get
+            Return _dblLength
+        End Get
+        Set(value As Double)
+            If value > 0 Then
+                _dblLength = value
+            Else
+                _dblLength = 0
+            End If
+        End Set
+    End Property
+
+    'Get and Set:
+    Public Property Width As Double
+        Get
+            Return _dblWidth
+        End Get
+        Set(value As Double)
+            If value > 0 Then
+                _dblWidth = value
+            Else
+                _dblWidth = 0
+            End If
+        End Set
+    End Property
+
+    'Get and Set:
+    Public Property Depth As Double
+        Get
+            Return _dblDepth
+        End Get
+        Set(value As Double)
+            If value > 0 Then
+                _dblDepth = value
+            Else
+                _dblDepth = 0
+
+            End If
+        End Set
+    End Property
+
+    Public Sub New()
+        _dblLength = 0
+        _dblDepth = 0
+        _dblWidth = 0
+    End Sub
+
+    Public Function GetVolume() As Double
+
+        Return _dblWidth * _dblLength * _dblDepth
+
+    End Function
+
+    Public Function GetGallons() As Double
+
+        Return GetVolume() * dblGAL_PER_CUBIC_FOOT
+
+    End Function
+
+End Class
